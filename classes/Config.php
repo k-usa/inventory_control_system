83d425678f747b4368429ce2d11a2b3adbbfcb3c
k-usa <you@example.com>
2021-07-29 09:17:50 +0200

    added updates

--- classes/Config.php
+++ classes/Config.php
@@ -3,18 +3,18 @@ session_start();
 class Config
 {
     // localhost
-    // private $servername ='localhost';
-    // private $username = 'root';
-    // private $password = '';
-    // private $db_name = 'inventory-control-system';
-    // public $conn;
+    private $servername ='localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db_name = 'inventory-control-system';
+    public $conn;
 
     // heroku
-    private $servername ='eu-cdbr-west-01.cleardb.com';
-    private $username = 'b9b3c9447ad231';
-    private $password = '278ab5c8';
-    private $db_name = 'heroku_8b827418e2ccc6d';
-    public $conn;
+    // private $servername ='eu-cdbr-west-01.cleardb.com';
+    // private $username = 'b9b3c9447ad231';
+    // private $password = '278ab5c8';
+    // private $db_name = 'heroku_8b827418e2ccc6d';
+    // public $conn;
 
     public function __construct()
     {
