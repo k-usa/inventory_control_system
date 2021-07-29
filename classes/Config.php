10dd91adb1104a09ca84a2c1be23200a584a937c
k-usa <you@example.com>
2021-07-29 10:03:36 +0200

    make it better

--- classes/Config.php
+++ classes/Config.php
@@ -9,11 +9,18 @@ class Config
     // private $db_name = 'inventory_control_db';
     // public $conn;
 
-    //heroku
-    private $servername ='eu-cdbr-west-01.cleardb.com';
-    private $username = 'b9b3c9447ad231';
-    private $password = '278ab5c8';
-    private $db_name = 'heroku_8b827418e2ccc6d';
+    //heroku --ver1
+    // private $servername ='eu-cdbr-west-01.cleardb.com';
+    // private $username = 'b9b3c9447ad231';
+    // private $password = '278ab5c8';
+    // private $db_name = 'heroku_8b827418e2ccc6d';
+    // public $conn;
+
+    //heroku --ver2
+    private $servername ='us-cdbr-east-04.cleardb.com';
+    private $username = 'bbca1315152850';
+    private $password = 'aa48e207';
+    private $db_name = 'heroku_7bcd1e58d86e369';
     public $conn;
 
     public function __construct()
